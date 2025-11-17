8a80341268cae29af742e97e238bcad3d1bd1a47
mateusLWD <alves.mateus1050@gmail.com>
2025-11-17 17:34:27 -0300

    images cache clear schedule added

--- pkg/main.go
+++ pkg/main.go
@@ -5,6 +5,7 @@ import (
 	external_service "github.com/Mateus-MS/Duo-Widget/modules/external/service"
 	widgets_images "github.com/Mateus-MS/Duo-Widget/modules/widget/images"
 	widget_repository "github.com/Mateus-MS/Duo-Widget/modules/widget/repository/local"
+	widget_schedules "github.com/Mateus-MS/Duo-Widget/modules/widget/schedules"
 	widget_service "github.com/Mateus-MS/Duo-Widget/modules/widget/service"
 	"github.com/Mateus-MS/Duo-Widget/routes"
 	"github.com/gin-gonic/gin"
@@ -20,6 +21,8 @@ func main() {
 
 	externalService := external_service.New(external_repository.New())
 	widgetService := widget_service.New(widget_repository.New(), *widgetRaw, externalService)
+
+	widget_schedules.StartCleanCacheSchedule(widgetService)
 
 	routes.Init(router, widgetService, externalService)
 	router.Run(":9898")
