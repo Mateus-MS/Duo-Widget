56924b54918f303f121de54a4256776eb66e5604
mateusLWD <alves.mateus1050@gmail.com>
2025-11-18 12:44:39 -0300

    query parameters now are path parameters

--- pkg/modules/widget/routes/widget.go
+++ pkg/modules/widget/routes/widget.go
@@ -3,6 +3,7 @@ package widget_routes
 import (
 	"errors"
 	"net/http"
+	"strings"
 
 	widget_service "github.com/Mateus-MS/Duo-Widget/modules/widget/service"
 	"github.com/gin-gonic/gin"
@@ -10,22 +11,26 @@ import (
 
 func WidgetRoute(service widget_service.Iservice) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		username := c.Query("username")
+		username := c.Param("username")
 		if username == "" {
 			c.AbortWithError(http.StatusBadRequest, errors.New("Must provide your username"))
 			return
 		}
 
-		mood := c.Query("mood")
+		mood := c.Param("mood")
 		if mood == "" {
 			mood = "angry"
 		}
+		mood = strings.Replace(mood, ".png", "", 1)
 
 		data, err := service.Serve(username, mood)
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
 		}
-		c.Writer.Write(data)
+
+		// Configure the response header to be cached by github
+		c.Header("Cache-Control", "public, max-age=3600")
+		c.Data(http.StatusOK, "image/png", data)
 	}
 }
